e7e1e62e32de99450e285c404d6e0e596454eaae
viacheslav <slavakozlov@MacBook-Air--Slava.local>
2023-03-24 11:13:16 +0200

    add files

---
+++ GHFollowers/GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by viacheslav on 24.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
